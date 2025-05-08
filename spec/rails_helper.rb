291945ba92297f97f0f1505575a4b7d9203d4d3d
Set27 <matvei.2002@mail.ru>
2025-05-08 20:47:04 +0300

    Init

---
+++ spec/rails_helper.rb
@@ -0,0 +1,11 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+# require_relative '../config/environment'
+# abort("The Rails environment is running in production mode!") if Rails.env.production?
+# require_relative "support/factory_bot"
+
+# require 'rspec/rails'
+# require 'spree/testing_support/factories'
+
+ActiveRecord::Migration.maintain_test_schema!
